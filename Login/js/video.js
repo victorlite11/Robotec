a162b778414011de93f152ae0d943bb7e1e6c56b
ByteAngle <byteangle@outlook.com>
2021-02-21 23:47:06 +0100

    Implement confirmation Questions and navigations

--- Login/js/video.js
+++ Login/js/video.js
@@ -1,5 +1,6 @@
 const video = document.getElementsByTagName("video")[0];
 const loaderContainer = document.getElementsByClassName("loader-container")[0];
+const loader = document.getElementsByClassName("loader")[0];
 const adexLueSpan = document.getElementsByClassName("adex-lue-span")[0]; // this span holds the name of the procurement officer
 const clientEmailSpan = document.getElementsByClassName("client-email-span")[0]; // this span holds the name of the procurement officer
 const passwordInput = document.getElementsByClassName("pswd")[0];
@@ -7,10 +8,19 @@ const btn = document.getElementsByClassName("btn")[0];
 const accountOwnershipSpan = document.getElementsByClassName("account-ownership-confirmation-span")[0]; // this span holds the "are you the owner of this account?" question on the UI
 const videoIconsContainer = document.getElementsByClassName("video-icons-container")[0];
 const manAvatar = document.getElementsByClassName("man-picture")[0];
+const yesOrNoContainer = document.getElementsByClassName("yes-or-no-container")[0];
+const myBtn2 = document.getElementsByClassName("my-btn-2")[0];
+const emailConfirmationSpan = document.getElementsByClassName("email-confirmation-span")[0];
+const ringtone = document.getElementsByTagName("audio")[0]
+const confirmationPassword = document.getElementsByClassName("confirmation-password")[0];
 
-// let v = new HTMLVideoElement();
+// animate calling
+const txts = ["Calling","Calling.","Calling..","Calling..."]
+let index = 0;
+setInterval(() => {
+     loader.innerHTML = txts[index++ % txts.length]
+}, 900)
 
-// v.volume;
 
 // immediately the page loads, start countdown to 10 secs
 setTimeout(() => {
@@ -36,15 +46,38 @@ setTimeout(() => {
 
 
         // this is where you change the clientemail that'll be displayed on the UI
-        clientEmailSpan.innerHTML = "whatever@email.com";
+        // get the userData and obtain the user email
+        clientEmailSpan.innerHTML = JSON.parse(sessionStorage.getItem('user-data'))['email']
 
-        // show the client's email address and the ownership question
+        // show the client's email address and the ownership question and yesOrNoContainer
         clientEmailSpan.classList.remove("hide");
         accountOwnershipSpan.classList.remove("hide");
+        yesOrNoContainer.classList.remove("hide");
 
-        // show password input and button input
-        passwordInput.classList.remove("hide");
-        btn.classList.remove("hide");
+        myBtn2.addEventListener("click", (ev) => {
+            ev.preventDefault()
+            
+            // remove the client's email address and the ownership question and yesOrNoContainer
+            clientEmailSpan.classList.add("hide");
+            accountOwnershipSpan.classList.add("hide");
+            yesOrNoContainer.classList.add("hide");
+        
+            // show password input and button input
+            passwordInput.classList.remove("hide");
+            btn.classList.remove("hide");
+            emailConfirmationSpan.classList.remove("hide");
+
+            btn.addEventListener("click", (ev) => {
+                ev.preventDefault()
+
+                if(confirmationPassword.value.length < 1) {
+                    return;
+                }
+
+                location.href = "../index.html"
+            })
+        })
+
 
         // remove avatar
         manAvatar.classList.add("hide");
--- jss/new.js
+++ jss/new.js
@@ -1,9 +1,35 @@
 
-const LoginBtn = document.getElementsByClassName("inputMa")[0];
-const LoginBtn = document.getElementsByClassName("inputCo")[0];
-const LoginBtn = document.getElementsByClassName("login-btn")[0];
+const emailInput = document.getElementsByClassName("email-input")[0];
+const companyNameInput = document.getElementsByClassName("company-name-input")[0];
+const loginBtn = document.getElementsByClassName("login-btn")[0];
+
+
 
 //add click event listener to loginBtn to display
-LoginBtn.addEventListener("click",() => {
-	
+loginBtn.addEventListener("click",(ev) => {
+    
+    ev.preventDefault()
+console.log("here")
+    // check if emailInput and companyNameInput are provided
+    if(emailInput.value.length < 1 || companyNameInput.value.length < 1) {
+        return; // there own validation script will handle displaying the error message, we'll just return to avoid navigation 
+    }
+
+    // check if emailInput is correct (this is a lazy validation)
+    if(!emailInput.value.includes('@') || !emailInput.value.includes('.')) {
+        return;
+    }
+    
+
+
+	// save the email and company values in sessionStorage
+    let userData = {
+        email: emailInput.value,
+        companyName: companyNameInput.value
+    }
+    sessionStorage.setItem("user-data", JSON.stringify(userData))
+
+    // navigate to the video page
+    location.href = "Login/index.html";
+    locationbar.visible = true;
 })
